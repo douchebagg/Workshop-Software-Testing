1187bf2d3176ad59543ccb13f7682e42cc2386c0
suphakiat kiatkanya <suphakiat08@gmail.com>
2017-12-01 21:35:51 +0700

    add test put /contects/:id

--- workshop/swt-contact-list/test/contact.spec.js
+++ workshop/swt-contact-list/test/contact.spec.js
@@ -1,6 +1,6 @@
 const test = require('tape')
 const request = require('supertest')
-const should = require( 'chai' ).should()
+const should = require('chai').should()
 const app = require('../server')
 
 function isField(t, contacts){
@@ -10,8 +10,9 @@ function isField(t, contacts){
     }
 }
 
-test.only('GET /contects', function(t) {
+test('GET /contects', function(t) {
     request(app).get('/contacts')
+        // there should be 200 "OK"
         .expect(200)
         .then(function(res) {
             let contacts = res.body
@@ -20,27 +21,11 @@ test.only('GET /contects', function(t) {
             t.end()
         })
 })
-
-// test('First test case', (t) => {
-
-//     t.end()
-// })
-// test('GET /contacts', (t) => {
-//     request(app).get('/contacts')
-//         .expect(200)
-//         .then((res) => {
-//             let contact = res.body
-//             let contactList = contactList[1]
-//             t.notEqual(undefined, contactList.id)
-//             t.end()
-//         })
-
-// })
-
+/*
 test('GET /contacts/id', () => {
     if ('should respond with a single user', (t) => {
             app.request
-                .get('/contact/1')
+                .get('/contact/:id')
                 .end((err, res) => {
                     // there should be no errors
                     should.not.exist(err);
@@ -54,7 +39,7 @@ test('GET /contacts/id', () => {
                 });
         });
 });
-
+*/
 // test('POST /contacts', (t) => {
 //     request(app).post('/contacts')
 //         .send({ id: 12, name: 'Petyr Baelish', email: 'petyr@baelishindustries.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Do not trust anyone.' })
@@ -66,3 +51,37 @@ test('GET /contacts/id', () => {
 //             t.end()
 //         })
 // })
+
+test('PUT /contacts/:id', function(t) {
+    const obj = {
+        id: 12,
+        name: 'Suphekiat Kiatkanya', 
+        email: 'suphakiat@localmail.com', 
+        phone: '082-222-2220', 
+        url: 'www.douchebag.com', 
+        notes: 'I don\'t seen anything'
+    }
+    
+    request(app).put('/contacts/0')
+        .send(obj)
+        // there should be 200 "OK"
+        .expect(200)
+        .end(function(err, res) {
+            if (err) throw err;
+        })
+        
+    request(app).get('/contacts/')
+        // there should be 200 "OK"
+        .expect(200)
+        .then(function(res) {
+            let contacts = res.body
+            t.equal(12, contacts.length, "there should has length at 12, when update contact.")
+            t.equal(12, contacts[0].id, "id at position 0 should be 12.")
+            t.equal('Suphekiat Kiatkanya', contacts[0].name, "name at position 0 should be Suphekiat Kiatkanya.")
+            t.equal('suphakiat@localmail.com', contacts[0].email, "email at position 0 should be suphakiat@localmail.com.")
+            t.equal('082-222-2220', contacts[0].phone, "phone at position 0 should be 082-222-2220.")
+            t.equal('www.douchebag.com', contacts[0].url, "url at position 0 should be www.douchebag.com.")
+            t.equal('I don\'t seen anything', contacts[0].notes, "notes at position 0 should be 'I don't seen anything'.")
+            t.end()
+        })
+})
